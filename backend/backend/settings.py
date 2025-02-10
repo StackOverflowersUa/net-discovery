f39f735d07687e4c2117a4bf8b48582dfb657317
ZrobUA <124803700+ZrobUA@users.noreply.github.com>
2025-02-10 22:55:00 +0200

    Bug fix. Non-critical bug with static files for django admin panel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,9 +123,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+FORCE_SCRIPT_NAME = "/api"
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # For local development
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
