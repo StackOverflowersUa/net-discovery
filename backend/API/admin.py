fa7658c8ad7e8599981576e312b2eade01d004fb
ZrobUA <124803700+ZrobUA@users.noreply.github.com>
2025-02-11 19:15:37 +0200

    Small fix

--- backend/API/admin.py
+++ backend/API/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Quest)
+admin.site.register(QuestMedia)
+admin.site.register(QuestRating)
+admin.site.register(QuestProgress)
+admin.site.register(QuestTask)
+admin.site.register(Leaderboard)
+admin.site.register(TaskOption)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework.routers import DefaultRouter
 from API import views
 
@@ -35,3 +38,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
